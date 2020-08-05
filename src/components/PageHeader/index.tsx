25b7946c18dfc7227579648c4525e25272bda117
Daniel Cunha <danielcc.jp@gmail.com>
2020-08-05 10:48:32 -0300

    teacherForm/List && Inputs

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 import logoImg from "../../assets/images/logo.svg";
 import backIcon from "../../assets/images/icons/back.svg";
 
-import "./styles.css"
+import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -20,10 +21,10 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
           </Link>
           <img src={logoImg} alt="Proffy" />
         </div>
-
         <div className="header-content">
           <strong>{props.title}</strong>
           {/* Propriedade automática */}
+          {props.description && <p>{props.description}</p>}
           {props.children}
         </div>
       </header>
